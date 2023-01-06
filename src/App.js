0a9eccacd72f6f4fb245dcc5887fc17b7fc3710f
abayasaye <abay400@gmail.com>
2023-01-06 15:47:37 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import "./index.css";
-import Home from './routes/Home';
-import About from './routes/About';
-import Project from './routes/Project';
-import Contact from './routes/Contact';
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./components/SignIn";
 
 function App() {
-  return(
+  return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/Project" element={<Project/>}/>
-      <Route path="/About" element={<About/>}/>
-      <Route path="/Contact" element={<Contact/>}/>
-      <Route path="/SignIn" element={<SignIn/>}/>
-      
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
     </>
   );
 }
