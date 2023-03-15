9145b3ae0eed8201a3e1579c5c729af53d229fee
abayasaye <abay400@gmail.com>
2023-03-15 20:09:26 +0200

    a

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,16 +8,16 @@ const Work = () => {
     <div className="work-container">
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
-        {WorkCardData.map((val,ind)=>{
-            return(
-                <WorkCard
-                key={ind}
-                imgsrc={val.imgsrc}
-                title={val.title}
-                text={val.text}
-                view={val.view}
-                />
-            )
+        {WorkCardData.map((val, ind) => {
+          return (
+            <WorkCard
+              key={ind}
+              imgsrc={val.imgsrc}
+              title={val.title}
+              text={val.text}
+              view={val.view}
+            />
+          );
         })}
       </div>
     </div>
